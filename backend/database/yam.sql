b90981ff39ecf8abbffed0c6839a3e60b3b8046c
Nicolas Simmonds <simmonds.nicolas@gmail.com>
2024-06-19 17:05:39 -0400

    modified config and db seeding files.

--- backend/database/yam.sql
+++ backend/database/yam.sql
@@ -7,12 +7,3 @@ CREATE DATABASE yam;
 
 \i yam-schema.sql
 \i yam-seed.sql
-
-\echo 'Delete and recreate yam_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
-
-DROP DATABASE IF EXISTS yam_test;
-CREATE DATABASE yam_test;
-\connect yam_test
-
-\i yam-schema.sql
